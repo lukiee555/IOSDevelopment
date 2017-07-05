0bc791d4e11009c57bf4003f11a2d7c33efd2302
lukiee555 <sonilokesh24@gmail.com>
2017-07-05 14:38:38 +0530

    Egg Timer Completed

--- MenuBar/MenuBar/ViewController.swift
+++ MenuBar/MenuBar/ViewController.swift
@@ -9,7 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+        var timer = Timer()
+        var showingtime = 210
+    
+    func decreaseTimer(){
+        if showingtime > 0 {
+            showingtime-=1
+            labelTime.text = String(showingtime)
+        }
+        else{
+            timer.invalidate()
+        }
+    }
+   
+    
+    @IBOutlet weak var labelTime: UILabel!
+    
+    @IBAction func buttonSubTen(_ sender: Any) {
+       
+        if showingtime > 10 {
+        showingtime -= 10
+        labelTime.text = String(showingtime)
+        }
+        else {
+            showingtime = 0
+             labelTime.text = String(showingtime)
+        }
+      
+    }
+    
+    @IBAction func buttonAddTen(_ sender: Any) {
+        showingtime+=10
+        labelTime.text = String(showingtime)
+    }
+    @IBAction func buttonReset(_ sender: Any) {
+        showingtime = 210
+        labelTime.text = String(showingtime)
+        
+    }
+    @IBAction func buttonStop(_ sender: Any) {
+        timer.invalidate()
+    }
+    @IBAction func buttonStart(_ sender: Any) {
+          timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.decreaseTimer), userInfo: nil, repeats: true)
+    }
+   
+    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
